f23f86ee00be64a118a3ff941542a325040e880b
Герман <buhrus2@yandex.ru>
2020-01-10 15:22:24 +0400

    first commit

---
+++ site.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$characterName = "Tom";
+$characterAge = 80;
+echo "There once was a man named $characterName <br>";
+echo "He was $characterAge years old <br>";
+echo "He really liked the name $characterName <br>";
+echo "But didn't like being $characterAge <br>";
+?>
+
+</body>
+</html>
